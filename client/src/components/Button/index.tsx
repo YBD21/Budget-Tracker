390a3e410b43a34a2b7eca57661dc8ea9e3041dc
YBD21 <santosh.deuja2000@gmail.com>
2024-06-20 13:49:05 +0545

    [client] convert Login Compoenet to typesafe format

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,31 +1,40 @@
-function Button({ title, handleClick, isDisable, type, small, isPending }) {
-  let buttonStyle
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
+
+import { MouseEventHandler } from "react";
+
+type ButtonProps = {
+  title: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  isDisable?: boolean;
+  type?: "primary";
+  small?: boolean;
+  isPending?: boolean;
+};
+
+function Button({ title, handleClick, isDisable, type, small, isPending }: ButtonProps) {
+  let buttonStyle;
   switch (type) {
-    case 'primary':
-      buttonStyle = `${'bg-black text-white focus:ring-black active:ring-black hover:ring-black dark:bg-neutral-600 dark:focus:ring-neutral-400 dark:active:ring-neutral-400 dark:hover:ring-neutral-400 dark:border dark:border-neutral-400'}`
-      break
+    case "primary":
+      buttonStyle = `${"bg-black text-white focus:ring-black active:ring-black hover:ring-black dark:bg-neutral-600 dark:focus:ring-neutral-400 dark:active:ring-neutral-400 dark:hover:ring-neutral-400 dark:border dark:border-neutral-400"}`;
+      break;
 
     default:
-      buttonStyle = `${'border border-gray-500 hover:border-gray-600  bg-gray-100 text-black focus:ring-gray-600 active:ring-gray-600 hover:ring-gray-500'}`
-      break
+      buttonStyle = `${"border border-gray-500 hover:border-gray-600  bg-gray-100 text-black focus:ring-gray-600 active:ring-gray-600 hover:ring-gray-500"}`;
+      break;
   }
   return (
     <button
       disabled={isDisable || isPending}
       id="button"
       onClick={handleClick}
-      className={`${buttonStyle} ${
-        small ? 'text-sm' : 'text-base'
-      }  w-full px-5  tracking-wide
- font-medium rounded-lg  text-center mr-2 mb-2
-focus:outline-none focus:ring-2  focus:ring-opacity-50 active:ring-4  active:ring-opacity-50 hover:ring-2 hover:ring-opacity-50  ${isDisable ? 'cursor-not-allowed opacity-60 py-2' : isPending ? 'cursor-progress py-1.5' : 'cursor-pointer py-2'}
+      className={`${buttonStyle} ${small ? "text-sm" : "text-base"}  mb-2 mr-2  w-full rounded-lg px-5  text-center font-medium tracking-wide hover:ring-2 hover:ring-opacity-50  focus:outline-none focus:ring-2  focus:ring-opacity-50 active:ring-4 active:ring-opacity-50  ${isDisable ? "cursor-not-allowed py-2 opacity-60" : isPending ? "cursor-progress py-1.5" : "cursor-pointer py-2"}
       `}
     >
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center">
         {isPending && (
           <svg
             fill="none"
-            className="w-8 h-8 animate-spin"
+            className="size-8 animate-spin"
             viewBox="0 0 32 32"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -40,7 +49,7 @@ focus:outline-none focus:ring-2  focus:ring-opacity-50 active:ring-4  active:rin
         {title}
       </div>
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,47 +1,50 @@
-'use client'
+"use client";
+import { ChangeEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as Yup from 'yup'
+import { FORGOTPASSWORD, HOME, SIGNUP } from "@/constants/Routes";
 
-import { FORGOTPASSWORD, HOME, SIGNUP } from '@/constants/Routes'
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import Button from "../Button";
+import ErrorMessage from "../ErrorMessage";
+import ToggleTheme from "../ToggleTheme";
+import { useAuthUser } from "@/hooks/user/useAuthUser";
+import LoginErrorBox from "./LoginErrorBox";
 
-import VisibilityIcon from '@mui/icons-material/Visibility'
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-// import CheckBox from '../Checkbox'
-import Button from '../Button'
-import ErrorMessage from '../ErrorMessage'
-import ToggleTheme from '../ToggleTheme'
-import { useAuthUser } from '@/hooks/user/useAuthUser'
-import LoginErrorBox from './LoginErrorBox'
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [open, setOpen] = useState(false)
-  const [error, setError] = useState(null) // capture error with this state
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(null); // capture error with this state
 
-  const { loginUserMutation } = useAuthUser()
+  const { loginUserMutation } = useAuthUser();
 
   // handle toggle to show or hide password
   const toggle = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   const formSchema = Yup.object({
-    email: Yup.string().email().required('Email is required !'),
+    email: Yup.string().email().required("Email is required !"),
     password: Yup.string()
       .trim()
-      .max(16, 'Password must be max 16 characters')
-      .required('Password is required !'),
-  })
+      .max(16, "Password must be max 16 characters")
+      .required("Password is required !"),
+  });
 
   const validationOpt = {
     resolver: yupResolver(formSchema),
-  }
+  };
 
   const {
     register,
@@ -49,60 +52,61 @@ const Login = () => {
     setValue,
     clearErrors,
     formState: { errors },
-  } = useForm(validationOpt)
+  } = useForm(validationOpt);
 
   // Function to handle password input change
-  const handlePasswordChange = (e) => {
-    const trimmedValue = e.target.value.trim()
-    setValue('password', trimmedValue)
-    handleClearErrors()
-  }
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const trimmedValue = e.target.value.trim();
+    setValue("password", trimmedValue);
+    handleClearErrors();
+  };
 
   const handleClearErrors = () => {
     // Clear errors when input value changes
-    clearErrors()
-  }
+    clearErrors();
+  };
 
-  const handleLoginSubmit = async (data) => {
-    setError(null) // initial on every click
+  const handleLoginSubmit: SubmitHandler<Inputs> = async (data) => {
+    setError(null); // initial on every click
     const loginData = {
       email: data.email,
       password: data.password,
-    }
+    };
 
     try {
-      const respond = await loginUserMutation.mutateAsync(loginData)
+      const respond = await loginUserMutation.mutateAsync(loginData);
       if (respond.status) {
-        router.replace(HOME)
+        router.replace(HOME);
       }
-    } catch (err) {
-      const errorMessage = err?.response?.data?.error_message || err?.message
-      setError(errorMessage)
+    } catch (err: any) {
+      const errorMessage = err?.response?.data?.error_message || err?.message;
+      setError(errorMessage);
       // console.log(errorMessage) // Network Error
     }
-  }
+  };
 
   return (
-    <div className="flex flex-col justify-center min-h-screen overflow-hidden bg-white dark:bg-neutral-800">
-      <div className="relative w-full p-6 mb-auto mx-auto mt-32 rounded-md sm:max-w-lg">
+    <div className="flex min-h-screen flex-col justify-center overflow-hidden bg-white dark:bg-neutral-800">
+      <div className="relative mx-auto mb-auto mt-32 w-full rounded-md p-6 sm:max-w-lg">
         <ToggleTheme />
-        <h2 className="text-3xl font-semibold text-center text-black dark:text-neutral-300">
+        <h2 className="text-center text-3xl font-semibold text-black dark:text-neutral-300">
           Login
         </h2>
         {/* login submit */}
         <form className="mt-3" onSubmit={handleSubmit(handleLoginSubmit)}>
-          <div className={`${errors?.email ? 'pb-2' : 'pb-3'}`}>
+          <div className={`${errors?.email ? "pb-2" : "pb-3"}`}>
             <label className="block text-sm font-semibold text-gray-800 dark:text-neutral-300">
               Email
             </label>
             <input
-              {...register('email')}
+              {...register("email")}
               type="email"
               autoComplete="email"
               onChange={handleClearErrors}
+              // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
               className={`
-              ${errors?.email ? 'border-red-400 focus:border-red-400 focus:ring-red-400' : 'border-black focus:border-black focus:ring-black dark:border-neutral-400 dark:focus:border-neutral-500 dark:focus:ring-neutral-400'}
-              block w-full px-4 py-1.5 mt-2.5  border-2 rounded-md  focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-neutral-700`}
+              ${errors?.email ? "border-red-400 focus:border-red-400 focus:ring-red-400" : "border-black focus:border-black focus:ring-black dark:border-neutral-400 dark:focus:border-neutral-500 dark:focus:ring-neutral-400"}
+              mt-2.5 block w-full rounded-md border-2  px-4 py-1.5  focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-neutral-700`}
             />
             <ErrorMessage errorName={errors?.email} />
           </div>
@@ -115,19 +119,20 @@ const Login = () => {
               Password
             </label>
 
-            <div className="relative flex flex-col cursor-pointer">
+            <div className="relative flex cursor-pointer flex-col">
               <input
-                {...register('password')}
-                type={open === false ? 'password' : 'text'}
+                {...register("password")}
+                type={open === false ? "password" : "text"}
                 autoComplete="current-password"
                 onChange={handlePasswordChange}
+                // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
                 className={`
-                ${errors?.password ? 'border-red-400 focus:border-red-400 focus:ring-red-400' : 'border-black focus:border-black focus:ring-black dark:border-neutral-400 dark:focus:border-neutral-500 dark:focus:ring-neutral-400'}
-                block w-full px-4 py-1.5 mt-2.5 text-black border-2 rounded-md  focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-neutral-700 dark:text-white`}
+                ${errors?.password ? "border-red-400 focus:border-red-400 focus:ring-red-400" : "border-black focus:border-black focus:ring-black dark:border-neutral-400 dark:focus:border-neutral-500 dark:focus:ring-neutral-400"}
+                mt-2.5 block w-full rounded-md border-2 px-4 py-1.5 text-black  focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-neutral-700 dark:text-white`}
               />
               {/* hide/unhide password */}
               <div
-                className={`absolute text-2xl top-2.5 right-3.5 ${errors.password ? 'text-red-700' : 'text-black dark:text-white'} `}
+                className={`absolute right-3.5 top-2.5 text-2xl ${errors.password ? "text-red-700" : "text-black dark:text-white"} `}
               >
                 {open === false ? (
                   <VisibilityIcon onClick={toggle} fontSize="small" />
@@ -140,7 +145,7 @@ const Login = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-center mt-2">
+          <div className="mt-2 flex items-center justify-center">
             {/* <CheckBox //handleClick={setCookies}
               title="Remember me"
             /> */}
@@ -148,7 +153,7 @@ const Login = () => {
             <div>
               <Link
                 href={FORGOTPASSWORD}
-                className="text-sm text-[#300] dark:text-gray-300 decoration-2 hover:underline"
+                className="text-sm text-[#300] decoration-2 hover:underline dark:text-gray-300"
               >
                 Forgot Password?
               </Link>
@@ -160,11 +165,7 @@ const Login = () => {
           {error && <LoginErrorBox message={error} />}
 
           <div className="pt-5">
-            <Button
-              title="Login"
-              type="primary"
-              isPending={loginUserMutation.isPending}
-            />
+            <Button title="Login" type="primary" isPending={loginUserMutation.isPending} />
           </div>
         </form>
 
@@ -173,7 +174,7 @@ const Login = () => {
             Not a member ? &nbsp;
             <Link
               href={SIGNUP}
-              className="font-semibold leading-6 text-[#300] dark:text-gray-300 hover:underline decoration-2"
+              className="font-semibold leading-6 text-[#300] decoration-2 hover:underline dark:text-gray-300"
             >
               Create Account
             </Link>
@@ -181,7 +182,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
