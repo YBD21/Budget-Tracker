712c4ba8765ced31a9ce5ad34583fd408c9f6c64
YBD21 <santosh.deuja2000@gmail.com>
2024-06-20 10:55:08 +0545

    [client] added types packages

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
 }
 
 export default nextConfig
