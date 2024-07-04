217599a961fc282a077ffed9aa3942ef9f5325f3
YBD21 <santosh.deuja2000@gmail.com>
2024-07-04 11:43:42 +0545

    [client] refactor toggleTheme

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,16 +1,24 @@
 'use client';
 
 import ToggleTheme from '../ToggleTheme';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
   return (
     <header className="sticky top-0 z-30 bg-gray-50 dark:bg-neutral-800 flex flex-row w-full drop-shadow-lg h-16">
-      <ToggleTheme />
-      <div className="flex w-1/2 mx-2">
-        <div className="flex items-center">
-          <strong className="mx-5"> Budget Tracker</strong>
+      {/* Title Name */}
+      <div className="flex w-1/2">
+        <div className="flex items-center mx-3.5">
+          <MenuIcon />
+          <p className="mx-4 text-base font-medium">Budget Tracker</p>
         </div>
       </div>
+      <ul className="flex justify-between w-full max-w-screen-lg mx-auto lg:max-w-full">
+        <ToggleTheme style={''} height={32} width={32} />
+        {/* Toggle Theme */}
+        {/* Notification */}
+        {/* Profile  */}
+      </ul>
     </header>
   );
 };
--- client/src/components/ToggleTheme/index.tsx
+++ client/src/components/ToggleTheme/index.tsx
@@ -1,51 +1,57 @@
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
-import { useTheme } from 'next-themes'
+import { FC, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-import darkModeIcon from '@/assets/darkmode.svg'
-import lightModeIcon from '@/assets/lightmode.svg'
-import { DARK_THEME, LIGHT_THEME } from '@/constants/actionName'
+import darkModeIcon from '@/assets/darkmode.svg';
+import lightModeIcon from '@/assets/lightmode.svg';
+import { DARK_THEME, LIGHT_THEME } from '@/constants/actionName';
 
-function ToggleTheme() {
-  const { resolvedTheme, setTheme } = useTheme()
+type ToggleThemeProps = {
+  style?: string;
+  height?: number;
+  width?: number;
+};
+
+const ToggleTheme: FC<ToggleThemeProps> = ({ style, height = 24, width = 24 }) => {
+  const { resolvedTheme, setTheme } = useTheme();
 
   // Initialize theme state using useState
-  const [theme, setThemeState] = useState('')
+  const [theme, setThemeState] = useState('');
 
   // Function to toggle theme
   const toggleTheme = () => {
-    const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME
-    setThemeState(newTheme)
+    const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
+    setThemeState(newTheme);
     // Update theme in localStorage
-    localStorage.setItem('theme', newTheme)
+    localStorage.setItem('theme', newTheme);
     // Set theme using next-themes hook
-    setTheme(newTheme)
-  }
+    setTheme(newTheme);
+  };
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem('theme')
+    const storedTheme = localStorage.getItem('theme');
 
     if (storedTheme) {
-      setThemeState(storedTheme)
+      setThemeState(storedTheme);
     } else {
       const newTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
         ? DARK_THEME
-        : LIGHT_THEME
-      setThemeState(newTheme)
+        : LIGHT_THEME;
+      setThemeState(newTheme);
       // Set theme using next-themes hook
-      setTheme(newTheme)
+      setTheme(newTheme);
     }
-  }, [resolvedTheme, setTheme])
+  }, [resolvedTheme, setTheme]);
 
   return (
-    <div className="absolute right-4 top-0 cursor-pointer">
+    <div className={`${style ? style : 'absolute right-4 top-0'} cursor-pointer`}>
       {theme === DARK_THEME ? (
         <Image
           suppressHydrationWarning
           src={darkModeIcon}
           alt="Dark Mode"
-          width={24}
-          height={24}
+          width={width}
+          height={height}
           onClick={toggleTheme}
         />
       ) : (
@@ -53,13 +59,13 @@ function ToggleTheme() {
           suppressHydrationWarning
           src={lightModeIcon}
           alt="Light Mode"
-          width={24}
-          height={24}
+          width={width}
+          height={height}
           onClick={toggleTheme}
         />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default ToggleTheme
+export default ToggleTheme;
