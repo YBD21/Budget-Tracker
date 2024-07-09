d0ed705ce16dc17f3085500a62f77cdb1f792092
YBD21 <santosh.deuja2000@gmail.com>
2024-07-09 20:05:33 +0545

    [client] added Overview placeholder

--- client/src/components/Dashboard/index.tsx
+++ client/src/components/Dashboard/index.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { useUserData } from '@/hooks/user/useUser';
+import Button from '../Button';
+import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
+import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import Add from '@mui/icons-material/Add';
 
 const Dashboard = () => {
   const { isPending: userLoading, data: currentUser } = useUserData();
@@ -10,12 +15,65 @@ const Dashboard = () => {
   }
 
   const firstName = currentUser?.firstName;
-  //  Income Icon -  import PaymentsIcon from '@mui/icons-material/Payments';
-  //  Expense Icon - import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-  // Total Balance - import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+
   return (
-    <div className="">
-      <span className="text-xl"> Hi,{firstName} 👋</span>
+    <div className="flex-col">
+      <div className="bg-gray-200 dark:bg-neutral-700 flex flex-row justify-between -m-4 px-3 pt-4 pb-1.5">
+        <p className="mx-2 text-xl"> Hi,{firstName} 👋</p>
+        <div className="flex mx-5">
+          <Button
+            title={
+              <div className="flex justify-between text-base dark:text-white">
+                <Add className="mr-2" fontSize="medium" /> Add
+              </div>
+            }
+            type="primary"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col mt-6 py-1.5">
+        <p className="mx-2 text-xl font-semibold">Overview</p>
+
+        <div className="flex justify-between my-6 mx-2">
+          {/* Income */}
+          <div className="flex px-4 py-1.5 border border-solid border-green-400 dark:border-green-700 shadow dark:shadow-sm rounded-lg">
+            <PaymentsOutlinedIcon
+              className="mr-2 text-green-500 dark:text-green-600"
+              fontSize="large"
+            />
+
+            <div className="flex flex-col mx-2">
+              <p className="text-sm font-semibold mb-0">Income</p>
+              <span className="text-lg tracking-wider text-green-700 dark:text-green-500">
+                Rs. 15,000.00
+              </span>
+            </div>
+          </div>
+          {/* Expense */}
+          <div className="flex px-4 py-1.5 border border-solid border-red-400 dark:red-green-700 shadow dark:shadow-sm rounded-lg">
+            <ReceiptLongOutlinedIcon
+              className="mr-2 text-red-500 dark:text-red-600"
+              fontSize="large"
+            />
+
+            <div className="flex flex-col mx-2">
+              <p className="text-sm font-semibold mb-0.5">Expense</p>
+              <span className="text-lg tracking-wider text-red-700 dark:text-red-500">
+                Rs. 1,000.00
+              </span>
+            </div>
+          </div>
+          {/* Balance */}
+          <div className="flex px-4 py-1.5 border border-solid border-gray-400 dark:border-stone-500 shadow dark:shadow-sm rounded-lg">
+            <AccountBalanceWalletOutlinedIcon className="mr-2" fontSize="large" />
+
+            <div className="flex flex-col mx-2">
+              <p className="text-sm font-semibold mb-0.5">Balance</p>
+              <span className="text-lg tracking-wider">Rs. 14,000.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/SideBar/SideBarItems.tsx
+++ client/src/components/SideBar/SideBarItems.tsx
@@ -3,47 +3,49 @@ import { Tooltip } from '@mui/material';
 import { SvgIconComponent } from '@mui/icons-material';
 
 type SideBarItemsProps = {
-  ActiveIcon: SvgIconComponent;
-  Icon: SvgIconComponent;
-  title: string;
-  isActive: boolean;
-  url: string;
-  isExpand: boolean;
+   ActiveIcon: SvgIconComponent;
+   Icon: SvgIconComponent;
+   title: string;
+   isActive: boolean;
+   url: string;
+   isExpand: boolean;
 };
 
 const SideBarItems = ({ ActiveIcon, Icon, title, isActive, url, isExpand }: SideBarItemsProps) => {
-  const router = useRouter();
+   const router = useRouter();
 
-  const redirect = () => {
-    router.push(url);
-  };
+   const redirect = () => {
+      router.push(url);
+   };
 
-  // if title is null tooltip is not shown
+   // if title is null tooltip is not shown
 
-  if (!isExpand) {
-    return (
-      <Tooltip title={title} followCursor>
-        <li
-          className={`flex flex-col items-center my-2.5 py-2.5 px-4 cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-xl ${isActive && 'bg-neutral-300 dark:bg-neutral-600'} shadow border border-solid border-gray-100 dark:border-stone-700 dark:shadow-lg`}
-          onClick={redirect}
-        >
-          {isActive ? <ActiveIcon /> : <Icon />}
-        </li>
-      </Tooltip>
-    );
-  }
+   if (!isExpand) {
+      return (
+         <Tooltip title={title} followCursor>
+            <li
+               className={`flex flex-col items-center my-2.5 py-2.5 px-4 cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-xl ${isActive && 'bg-neutral-300 dark:bg-neutral-600'} shadow border border-solid border-gray-100 dark:border-stone-700 dark:shadow-lg`}
+               onClick={redirect}
+            >
+               {isActive ? <ActiveIcon /> : <Icon />}
+            </li>
+         </Tooltip>
+      );
+   }
 
-  return (
-    <li
-      className={`flex justify-between items-center my-2.5 py-2.5 px-4 cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-xl ${isActive && 'bg-neutral-300 dark:bg-neutral-600'} shadow border border-solid border-gray-100 dark:border-stone-700 dark:shadow-lg`}
-      onClick={redirect}
-    >
-      <div className="flex items-center space-x-5">{isActive ? <ActiveIcon /> : <Icon />}</div>
-      <p className={`text-xs flex-grow text-center ${isActive ? 'font-semibold' : 'font-normal'}`}>
-        {title}
-      </p>
-    </li>
-  );
+   return (
+      <li
+         className={`flex justify-between items-center my-2.5 py-2.5 px-4 cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-xl ${isActive && 'bg-neutral-300 dark:bg-neutral-600'} shadow border border-solid border-gray-100 dark:border-stone-700 dark:shadow-lg`}
+         onClick={redirect}
+      >
+         <div className="flex items-center space-x-5">{isActive ? <ActiveIcon /> : <Icon />}</div>
+         <p
+            className={`text-xs flex-grow text-center ${isActive ? 'font-semibold' : 'font-normal'}`}
+         >
+            {title}
+         </p>
+      </li>
+   );
 };
 
 export default SideBarItems;
