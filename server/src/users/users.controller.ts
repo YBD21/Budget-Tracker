cdb74135e378c38e3a7d5950770581aeb87132ce
YBD21 <santosh.deuja2000@gmail.com>
2024-07-24 09:54:17 +0545

    [server] added get budget-list

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,19 +1,21 @@
-import {
-  //   Body,
-  Controller,
-  //   HttpCode,
-  //   HttpStatus,
-  //   Post,
-  //   Res,
-} from '@nestjs/common';
-// import { Response } from 'express';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
 
-@Controller('auth-system')
+@Controller('user')
 export class UsersController {
-  //   @HttpCode(HttpStatus.OK)
-  //   @Post('verify-captcha')
-  //   async verifyCaptcha(@Res() res: Response, @Body() recaptchaData: any) {
-  //     console.log(recaptchaData);
-  //     res.status(HttpStatus.CREATED).send(true);
-  //   }
+  @Get('budget-list:id')
+  async sendlist(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Query() query: any,
+  ) {
+    const { page, pageSize, sortField, sortOrder, filters } = query;
+    console.log('page :', page);
+    console.log('pageSize :', pageSize);
+    console.log('sortField :', sortField);
+    console.log('sortOrder :', sortOrder);
+    console.log('filters :', filters);
+    console.log(id);
+    return res.json('hello');
+  }
 }
