e17d10ac4cd90645ff9645ec722e7b4eb926c278
YBD21 <santosh.deuja2000@gmail.com>
2024-07-26 09:27:05 +0545

    [server] update user controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('user')
 export class UsersController {
-  @Get('budget-list:id')
-  async sendlist(@Res() res: Response, @Param() params: any) {
-    const { userId, page, pageSize, sortField, sortOrder, filters } = params;
-    console.log('page :', page);
-    console.log('pageSize :', pageSize);
-    console.log('sortField :', sortField);
-    console.log('sortOrder :', sortOrder);
-    console.log('filters :', filters);
-    console.log(userId);
+  @Get('budget-list/:userId')
+  async sendlist(
+    @Res() res: Response,
+    @Param('userId') userId: string,
+    @Query() query: any,
+  ) {
+    const { current, pageSize, sortField, sortOrder, type, reoccur } = query;
+
+    console.log('current:', current);
+    console.log('pageSize:', pageSize);
+    console.log('sortField:', sortField);
+    console.log('sortOrder:', sortOrder);
+    console.log('type:', type);
+    console.log('reoccur:', reoccur);
+    console.log('userId:', userId);
+
     return res.json('hello');
   }
 }
