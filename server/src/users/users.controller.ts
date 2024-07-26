489db521c24b1e6c1f3338403e809ad4d38910aa
YBD21 <santosh.deuja2000@gmail.com>
2024-07-26 09:16:33 +0545

    [server] modify user controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,21 +1,17 @@
-import { Controller, Get, Param, Query, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('user')
 export class UsersController {
   @Get('budget-list:id')
-  async sendlist(
-    @Res() res: Response,
-    @Param('id') id: string,
-    @Query() query: any,
-  ) {
-    const { page, pageSize, sortField, sortOrder, filters } = query;
+  async sendlist(@Res() res: Response, @Param() params: any) {
+    const { userId, page, pageSize, sortField, sortOrder, filters } = params;
     console.log('page :', page);
     console.log('pageSize :', pageSize);
     console.log('sortField :', sortField);
     console.log('sortOrder :', sortOrder);
     console.log('filters :', filters);
-    console.log(id);
+    console.log(userId);
     return res.json('hello');
   }
 }
