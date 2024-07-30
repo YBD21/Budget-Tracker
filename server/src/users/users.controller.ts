c887736fd3ec06324d8dc5cc51633bf734145db7
YBD21 <santosh.deuja2000@gmail.com>
2024-07-30 10:48:06 +0545

    [server] updated random generator

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -15,13 +15,13 @@ export class UsersController {
     console.log('reoccur:', reoccur);
     // console.log('userId:', userId);
 
-    const generateDataSource = (length: number) =>
+    const generateDataSource = (length) =>
       Array.from({ length }).map((_, i) => ({
         key: i,
         date: `2023-07-${i + 1}`,
         title: `Transaction ${i}`,
         type: i % 2 === 0 ? 'Income' : 'Expense',
-        reoccur: i % 2 === 0,
+        reoccur: i % 3 === 0 ? 'Year' : i % 3 === 1 ? 'Month' : 'Once', // Update reoccur to be 'Year', 'Month', or 'Once'
         amount: Math.floor(Math.random() * (99999 - 1000 + 1)) + 1000,
       }));
 
