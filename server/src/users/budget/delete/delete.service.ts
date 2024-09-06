0c1f7d023efa9713f5b5fa4e2cfe8292920ce423
YBD21 <santosh.deuja2000@gmail.com>
2024-09-06 19:49:58 +0545

    [server] added deleteBudgetRecord function

--- server/src/users/budget/delete/delete.service.ts
+++ server/src/users/budget/delete/delete.service.ts
@@ -5,4 +5,26 @@ import { FirebaseService } from 'src/firebase/firebase.service';
 export class DeleteBudgetService {
   private readonly logger = new Logger(DeleteBudgetService.name);
   constructor(private readonly firebaseService: FirebaseService) {}
+  private readonly usersCollectionPath = 'Users';
+  private readonly budgetEntryCollectionPath = 'BudgetEntry';
+
+  async deleteBudgetRecord(userId: string, recordId: string): Promise<boolean> {
+    const budgetEntryDataRef = this.firebaseService
+      .getFirestore()
+      .collection(this.usersCollectionPath)
+      .doc(userId)
+      .collection(this.budgetEntryCollectionPath);
+
+    try {
+      const documentRef = budgetEntryDataRef.doc(recordId);
+      const deleteStatus = await documentRef.delete();
+      console.log(deleteStatus);
+      return true;
+    } catch (error) {
+      this.logger.error(
+        `Error while deleting BudgetRecord from Firestore: ${error.message}`,
+      );
+      return false;
+    }
+  }
 }
